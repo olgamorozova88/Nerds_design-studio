e28619c4eef382c0fb5636b425700e95a3c2debd
Runa-knox <88970105+Runa-knox@users.noreply.github.com>
2021-11-21 21:19:29 +0500

    Popup opening via JS added

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('.modal');
+const modalShow = document.querySelector('.contacts-button');
+const modalHide = document.querySelector('.modal-close');
+
+modalShow.addEventListener('click', function () {
+  modal.classList.remove('modal-hide');
+  modal.classList.add('modal-show');
+});
+
+modalHide.addEventListener('click', function () {
+  modal.classList.remove('modal-show');
+  modal.classList.add('modal-hide');
+});
